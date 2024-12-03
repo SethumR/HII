5d37e76af9f53897a464209845f566a7ad094016
sasindri-siriwardene <sasindrisiriwardene@gmail.com>
2024-12-03 11:18:08 +0530

    intro

--- src/signup.jsx
+++ src/signup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function signup() {
   return (
-    <><div>signup</div><h1>MY name is sasindri</h1></>
+    <><div>signup</div><h1>MY name is sasindri siriwardene </h1></>
     
   )
 }
